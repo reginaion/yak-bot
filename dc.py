306d07a3572ba9b739856be25d930fd5a6c8e1a3
reginaion2476 <eora8210@gmail.com>
2021-06-14 02:45:41 +0800

    ver 0.0.1, date 210614, add time scheduling

---
+++ dc.py
@@ -0,0 +1,87 @@
+
+import discord
+import asyncio
+import datetime
+import pytz
+import os
+from discord.ext import commands
+from discord.ext import tasks
+
+client = commands.Bot(command_prefix=';')
+
+alarm_time = '01:22'#24hrs
+channel_id = 387998196422672386
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+"""
+@client.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@client.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@client.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@client.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@client.command()
+async def info(ctx):
+    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
+    
+    # give info about you here
+    embed.add_field(name="Author", value="<YOUR-USERNAME>")
+    
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(client.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+
+    await ctx.send(embed=embed)
+
+client.remove_command('help')
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
+
+    embed.add_field(name=";add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+    embed.add_field(name=";multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+    embed.add_field(name=";greet", value="Gives a nice greet message", inline=False)
+    embed.add_field(name=";cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+    embed.add_field(name=";info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name=";help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+"""
+# Close the bot
+@client.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("The bot will shutdown await a minute")
+    await ctx.bot.close()
+
+@tasks.loop(minutes=1)
+async def time_check():
+    cst = datetime.datetime.now(tz=pytz.timezone('Asia/Taipei')).time()
+    weekday = datetime.datetime.now(tz=pytz.timezone('Asia/Taipei')).weekday()
+    if weekday == 0 and cst.hour == 1 and cst.minute == 46:
+        await client.wait_until_ready()
+        channel = client.get_channel(channel_id)
+        await channel.send('```客家道場```')
+
+
+time_check.start()
+
+client.run(os.getenv('BOT_TOKEN'))
