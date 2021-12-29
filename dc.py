017200d6371ad4ab1cc3ee4a1e90ccc9c852e1bf
reginaion <37777445+reginaion@users.noreply.github.com>
2021-12-29 23:33:33 +0800

    ver 0.0.5, date 211229, new command

--- dc.py
+++ dc.py
@@ -1,135 +1,28 @@
+# welcome-bot
+## this bot will send a welcome message and assign a role
 
+# import all necessary commands and libraries
 import discord
 import asyncio
-import datetime
-import pytz
-import os
-from discord.ext import commands
-from discord.ext import tasks
-
 intents = discord.Intents(members=True)
 client=discord.Client(intents=intents)
-welcomechannel = await client.fetch_channel(702741572344610910)
-
-alarm_time = '23:03'#24hrs
-channel_id = 387998196422672386
-channel_id_2 = 925725103801630761
-channel_id_test = 925763452281159680
-
+welcomechannel = await client.fetch_channel(channel_id) # https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID
+# Make sure you get the ID of your channel by right-clicking it and clicking `Copy ID`. Make sure developer mode is on!
 @client.event
 async def on_ready():
-    activity = discord.Game(name="command list: [;help, ;shutdown]")
-    await client.change_presence(status=discord.Status.online, activity=activity)
-    #await client.change_presence(activity=activity)
-    #await client.change_presence(activity=discord.Streaming(name='Fortnite', url='https://www.twitch.tv/UR_TWITCH_GOES_HERE You cant do YT only Twitch.'))
-    print('Logged in as')
+    print('logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('------')
-
-@client.command()
-@commands.is_owner()
-async def send_2(ctx, *, message:str):
-    #channel = client.get_channel(channel_id)
-    channel = client.get_channel(channel_id_2)
-    await channel.send(message)
-
-@client.command()
-@commands.is_owner()
-async def send(ctx, *, message:str):
-    #channel = client.get_channel(channel_id)
-    channel = client.get_channel(channel_id)
-    await channel.send(message)
-
-
-@client.command()
-@commands.is_owner()
-async def send_t(ctx, *, message:str):
-    #channel = client.get_channel(channel_id)
-    channel = client.get_channel(channel_id_test)
-    await channel.send(message)
-"""
-@client.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
-
-@client.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
-
-@client.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
-@client.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-
-@client.command()
-async def info(ctx):
-    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
-    
-    # give info about you here
-    embed.add_field(name="Author", value="<YOUR-USERNAME>")
-    
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="Server count", value=f"{len(client.guilds)}")
-
-    # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
-
-    await ctx.send(embed=embed)
-
-client.remove_command('help')
-
-@client.command()
-async def help(ctx):
-    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
-
-    embed.add_field(name=";add X Y", value="Gives the addition of **X** and **Y**", inline=False)
-    embed.add_field(name=";multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
-    embed.add_field(name=";greet", value="Gives a nice greet message", inline=False)
-    embed.add_field(name=";cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
-    embed.add_field(name=";info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name=";help", value="Gives this message", inline=False)
-
-    await ctx.send(embed=embed)
-"""
-# Close the bot
-@client.command()
-@commands.is_owner()
-async def shutdown(ctx):
-    await ctx.send("The bot will shutdown await a minute")
-    await ctx.bot.close()
-
-@tasks.loop(minutes=1)
-async def time_check():
-    cst = datetime.datetime.now(tz=pytz.timezone('Asia/Taipei')).time()
-    weekday = datetime.datetime.now(tz=pytz.timezone('Asia/Taipei')).weekday()
-    if weekday == 6 and cst.hour == 22 and cst.minute == 30:
-        await client.wait_until_ready()
-        channel = client.get_channel(channel_id)
-        await channel.send('```客家道場```')
-    if weekday == 6 and cst.hour == 22 and cst.minute == 35:
-        await client.wait_until_ready()
-        channel = client.get_channel(channel_id_2)
-        await channel.send('```客家道場```')
-
-#Mod Leave Announcement
-"""
-@client.event
-async def on_member_remove(member):
-    print("Recognized that " + member.name + " left")
-    await client.send_message(discord.Object(id=702741572344610910), '**' + member.mention + '** just left.')
-    print("Sent message to #CHANNEL")
-
-
-@client.event
-async def on_message_join(member):
-    channel = client.get_channel(702741572344610910)
-    embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!") # F-Strings!
-    embed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
-    await channel.send(embed=embed)
+    print('-----')
+ # Customise the message below to what you want to send new users!
+newUserMessage = """
+You
+can
+put
+your
+multiline
+message
+here!
 """
 
 @client.event
@@ -158,7 +51,4 @@ async def on_member_leave(member):
         description="Until we meet again old friend." # A description isn't necessary, you can delete this line if you don't want a description.
         color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
     )
-
-time_check.start()
-
-client.run(os.getenv('BOT_TOKEN'))
+client.run('token')
