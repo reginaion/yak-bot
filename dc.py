d145247463957f4b243da9b9cb80e222283dea27
reginaion <37777445+reginaion@users.noreply.github.com>
2021-12-29 23:50:48 +0800

    ver 0.0.5, date 211229, new command

--- dc.py
+++ dc.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from discord.ext import tasks
 
 
-intents = discord.Intents()
+intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix=';', intents=intents)
 
