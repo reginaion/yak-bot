28c2172ab746f9b7a360670205a41125c7c711c5
reginaion <37777445+reginaion@users.noreply.github.com>
2021-12-29 23:40:56 +0800

    ver 0.0.5, date 211229, new command

--- dc.py
+++ dc.py
@@ -9,8 +9,8 @@ from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.members=True
+
 client = commands.Bot(command_prefix=';', intents=intents)
-welcomechannel = await client.fetch_channel(702741572344610910)
 
 alarm_time = '23:03'#24hrs
 channel_id = 387998196422672386
@@ -123,7 +123,7 @@ async def on_member_remove(member):
     print("Recognized that " + member.name + " left")
     await client.send_message(discord.Object(id=702741572344610910), '**' + member.mention + '** just left.')
     print("Sent message to #CHANNEL")
-
+"""
 
 @client.event
 async def on_message_join(member):
@@ -131,44 +131,6 @@ async def on_message_join(member):
     embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!") # F-Strings!
     embed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
     await channel.send(embed=embed)
-"""
-newUserMessage = """
-You
-can
-put
-your
-multiline
-message
-here!
-"""
-
-
-@client.event
-async def on_member_join(member):
-    print("Recognised that a member called " + member.name + " joined")
-    try: 
-        await client.send_message(member, newUserMessage)
-        print("Sent message to " + member.name)
-    except:
-        print("Couldn't message " + member.name)
-    embed=discord.Embed(
-        title="Welcome "+member.name+"!"
-        description="We're so glad you're here!"
-        color=discord.Color.green()
-    )
-        
-    role = discord.utils.get(member.server.roles, name="name-of-your-role") #  Gets the member role as a `role` object
-    await client.add_roles(member, role) # Gives the role to the user
-    print("Added role '" + role.name + "' to " + member.name)
-
-@client.event
-async def on_member_leave(member):
-    print("Recognised that a member called " + member.name + " left")
-    embed=discord.Embed(
-        title="😢 Goodbye "+member.name+"!",
-        description="Until we meet again old friend." # A description isn't necessary, you can delete this line if you don't want a description.
-        color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
-    )
 
 time_check.start()
 
