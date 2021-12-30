72485c6beea07723d32192864aa494ca14464b63
reginaion <37777445+reginaion@users.noreply.github.com>
2021-12-30 12:45:45 +0800

    ver 0.0.5, date 211229, new command

--- dc.py
+++ dc.py
@@ -33,13 +33,13 @@ role_emoji = ["<:geofforyA:925962558349934593>","<:dholeA:925962613718929490>"]
 @client.event
 async def on_member_join(member):
     if member.guild.id == invite_guild_id:
-        guild = client.get_guild(invite_guild_id)
-        role = discord.utils.get(guild.roles, name="遊客")
-        await member.add_roles(role)
-        
+        #guild = client.get_guild(invite_guild_id)
+        #role = discord.utils.get(guild.roles, name="遊客")
+        #await member.add_roles(role)
+
         channel = client.get_channel(invite_channel_id)
         #await member.send('Private message')
-        embed=discord.Embed(title=f"ようこそジャパリパークへ! {member.name}", description=f"感謝您加入 {member.guild.name}!\n請至<#925779385729032262>閱讀守則\n請至<#925732268197167125>索取身分組") # F-Strings!
+        embed=discord.Embed(title=f"ようこそジャパリパークへ! {member.name}", description=f"感謝您加入 {member.guild.name}!\n請至<#925779385729032262>閱讀守則\n請至<#925732268197167125>釘選處索取身分組") # F-Strings!
         embed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
         await channel.send(embed=embed)
 
