fe321d16fc73cb868affa5cf1e8a0625f8b4872a
reginaion <37777445+reginaion@users.noreply.github.com>
2023-02-12 22:26:06 +0800

    ver 0.0.8.5, date 230212, add on_message_delete

--- dc.py
+++ dc.py
@@ -268,12 +268,12 @@ async def ping(ctx):
 
 @client.command(name="check_version") # Test command which works
 async def check_version(ctx):
-    await ctx.send("ver 0.0.8.4, date 230205, edit invite message")
+    await ctx.send("ver 0.0.8.5, date 230212, add on_message_delete")
 
 @client.event
 async def on_message_delete(message):
     channel = client.get_channel(1074333559596273715)
-    await channel.send(f'message: {message.content} by {message.author} was deleted in {message.channel}')
+    await channel.send(f'message: <#{message.channel}> {message.content}: {message.author}')
 
 #@client.slash_command(guild_ids=[702741572344610907])
 #async def hello(ctx):
