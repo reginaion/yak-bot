e2ec0980f48b0403f1c5f704dbc57a3ac466ccf4
reginaion <37777445+reginaion@users.noreply.github.com>
2023-09-03 21:14:53 +0800

    ver 0.0.9.14, date 230903, add jp info

--- dc.py
+++ dc.py
@@ -704,23 +704,24 @@ async def on_raw_reaction_add(payload):
                         check = 1
 
             if check == 0:
+                await entry_msg.clear_reactions()
                 return
 
             if payload.channel_id == svr.channel_id['invite']:
                 mention_msg   = f'<@{member.id}>\n'
                 jp_msg        = svr.jp_message_on_member_update
                 if [i.id for i in member.roles].count(svr.no_welcome_msg_role_id) == 0:
-                    await entry_channel.send(content=(mention_msg+jp_msg))
+                    await entry_channel.edit(content=(mention_msg+jp_msg))
                 await payload.member.send(content=jp_msg)
-                await entry_msg.delete()
+                await entry_msg.clear_reactions()
                 return
 
             elif payload.channel_id == svr.channel_id['help']:
                 mention_msg   = f'<@{member.id}>\n'
                 jp_msg        = svr.jp_message_on_member_join
-                await entry_channel.send(content=(mention_msg+jp_msg))
+                await entry_channel.edit(content=(mention_msg+jp_msg))
                 await payload.member.send(content=jp_msg)
-                await entry_msg.delete()
+                await entry_msg.clear_reactions()
                 return
 
             return
